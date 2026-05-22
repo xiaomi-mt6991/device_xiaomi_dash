5cc5b9f29ed38d9b9618c2dd89d531889417ca6c
Ben Grisdale <bengris32@protonmail.ch>
2026-05-22 08:52:39 +0200

    dash: Switch to MediaTek boot control service
    Change-Id: I170856f63b165a708d64f62c14bf408b5f3b3d48
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>
    Signed-off-by: therealmharc <therealmharc@gmail.com>

--- device.mk
+++ device.mk
@@ -31,8 +31,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
-    com.android.hardware.boot \
-    android.hardware.boot-service.default_recovery
+    android.hardware.boot-service.mediatek \
+    android.hardware.boot-service.mediatek_recovery
 
 PRODUCT_PACKAGES += \
     create_pl_dev \
